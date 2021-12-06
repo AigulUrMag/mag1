6c047de63961a983f7f3ced44d56454bc4566ad6
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-06 16:03:47 +0500

    Add files via upload

--- 6_2.py
+++ 6_2.py
@@ -1,14 +1,20 @@
 import time
 from threading import Thread
-
+from datetime import datetime
 
 def get_thread(thread_name):
     time.sleep(1)
     print(thread_name,'поток')
 
+t1=datetime.now()
+for i in range(5):
+    get_thread(i+1)
+print('time',(datetime.now()-t1).microseconds)    
 
+t2=datetime.now()
 threads = [Thread(target=get_thread(i + 1), args=(i + 1)) for i in range(5)]
 for t in threads:
     t.start()
 for t in threads:
     t.join()
+print('time',(datetime.now()-t2).microseconds)    
