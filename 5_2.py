e317d32d280ac45860e2523bc7fa46f8f9389f7d
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-06 11:16:16 +0500

    Update 5_2.py

--- 5_2.py
+++ 5_2.py
@@ -3,20 +3,20 @@ class Point():
 		self.x=x
 		self.y=y
 	def __sub__(self,other) :
-		return Point(abs(other.x-self.x),abs(other.y-self.y))
+		return Point(other.x-self.x,other.y-self.y)
 	def __add__(self, other):
 		return Point(self.x + other.x, self.y + other.y)
 	def __mul__(self, other):
 		return self.x * other.x + self.y * other.y
-	def len(self):
-		return (self.x ** 2 + self.y ** 2) ** 0.5
-p1=Point(1,9)	
-p2=Point(4,6)
+	def len(self,other):
+		return (((other.x-self.x)**2)+((other.y-self.y)**2))**0.5
+p1=Point(1,1)	
+p2=Point(2,2)
 r1=p2-p1
 print(r1.x,r1.y)
 r2=p1+p2
 print(r2.x,r2.y)
 r3=p2*p1
 print(r3)
-print(p1.len())
+print(p2.len(p1))
 
