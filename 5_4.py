44ba0df714f307fb4fd371efa8f369b1c94ba6d0
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-06 12:27:27 +0500

    Update 5_4.py

--- 5_4.py
+++ 5_4.py
@@ -1,21 +1,29 @@
 class Voin():
-	
-	health=100
-	#health2=100
-	
+	def __init__(self,health): 
+		self.health=health
+			
 	def ataka(self) :
-		if health>20:
+		if self.health>20:
 			self.health-=20
-
-unit1=Voin()
-unit2=Voin()
-print(unit1.health)
-print(unit2.health)
-#while unit1.ataka.health2>0
-n=input("Введите 1 или 2")
-if n==1 :
-	unit2.ataka()
-	print("Атаковал первый,осталось здоровья у второго ",unit2.health)
+			return self.health
+		else :	
+			self.health=0
+		return self.health	
+unit1=Voin(100)
+unit2=Voin(100)
+while unit2.health!=0 and unit1.health!=0 :
+	n=input("Введите 1 или 2 ")
+	if n=='1' :
+		print(n)
+		unit2.ataka()
+		print("Атаковал первый,осталось здоровья у второго ",unit2.health)
+	else :
+		print(n)
+		unit1.ataka()
+		print("Атаковал второй,осталось здоровья у первого", unit1.health)
 else :
-	unit1.ataka()
-	print("Атаковал второй,осталось здоровья у первого", unit1.health)
+	print("Игра закончена")	
+if unit2.health==0 		:
+	print("Выиграл первый игрок")	
+else:
+	print("Выиграл второй игрок")		
