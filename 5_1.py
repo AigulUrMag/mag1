56fa5b14fb5860c0de38017d97671fd19527ada2
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-06 10:40:08 +0500

    Update 5_1.py

--- 5_1.py
+++ 5_1.py
@@ -1,12 +1,12 @@
-class StringVar:
+lass StringVar:
 	def __init__(self,str1) :
 		self.str1=str1
 	def set(self,str1) :
-		return str1.upper()
+		self.str1=str1
+		return self.str1
 	def get(self,str1) :
 		return str1
 str2= StringVar("Hello ")
-print(str1.set("good by"))
-print(str1.get("Hello"))
-
+print(str2.set("good by"))
+print(str2.get("Hello"))
 
