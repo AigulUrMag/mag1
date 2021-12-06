f07488711b9fb16f866b81b2be5785eb9d5ece72
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-06 10:21:21 +0500

    Update 5_1.py

--- 5_1.py
+++ 5_1.py
@@ -1,11 +1,11 @@
-class StringVar(str):
-	def __init__(self,str) :
-		self.str=str
-	def set(self,str) :
-		return str.upper()
-	def get(self,str) :
-		return str
-str1= StringVar("Hello ")
+class StringVar:
+	def __init__(self,str1) :
+		self.str1=str1
+	def set(self,str1) :
+		return str1.upper()
+	def get(self,str1) :
+		return str1
+str2= StringVar("Hello ")
 print(str1.set("good by"))
 print(str1.get("Hello"))
 
