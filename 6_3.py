65829b78a204543b31e84f7dba064969d7a25acc
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-07 16:04:34 +0500

    Add files via upload

---
+++ 6_3.py
@@ -0,0 +1,21 @@
+import requests
+import time
+from threading import Thread
+from datetime import datetime
+def get_html(link):
+    response = requests.get(link)
+    print(link, len(response.text))
+
+urls=['https://google.ru]','https://yandex.ru]','https://mail.ru']
+t1=datetime.now()
+for i in urls:
+    get_html(i+1)
+print('time1',(datetime.now()-t1).microseconds)    
+
+t2=datetime.now()
+threads = [Thread(target=get_html(i + 1), args=(i + 1)) for i in urls]
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
+print('time2',(datetime.now()-t2).microseconds)    
