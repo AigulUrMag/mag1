815584167fd29aca66c31a97b7cfd5ae162840e2
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-12 15:38:01 +0500

    Update 6_3.py

--- 6_3.py
+++ 6_3.py
@@ -7,17 +7,15 @@ def get_html(link):
     print(link, len(response.text))
 
 urls=['https://google.ru','https://yandex.ru','https://mail.ru']
-t1=datetime.now()
+t1=time.time()
 for i in urls:
     get_html(i)
-print('time1',(datetime.now()-t1).microseconds)    
-print(time.clock())
+print('time1',(time.time()-t1).microseconds)    
 
-t2=datetime.now()
 threads = [Thread(target=get_html(i), args=(i)) for i in urls]
+t2=time.time()
 for t in threads:
     t.start()
 for t in threads:
     t.join()
-print('time2',(datetime.now()-t2).microseconds) 
-print(time.clock())
+print('time2',(time.time()-t2).microseconds)
