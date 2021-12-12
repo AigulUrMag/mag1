dc95f845cc741c8a1c2a02fd1bd450bb000c46a6
AigulUrMag <94735304+AigulUrMag@users.noreply.github.com>
2021-12-12 15:24:47 +0500

    Update 6_2.py

--- 6_2.py
+++ 6_2.py
@@ -4,15 +4,16 @@ from datetime import datetime
 
 def get_thread(thread_name):
     time.sleep(1)
-    print(thread_name,'поток')
+    print(f'{thread_name} поток')
 
 t1=datetime.now()
 for i in range(5):
     get_thread(i+1)
 print('time',(datetime.now()-t1).microseconds)    
 
+#t2=datetime.now()
+threads = [Thread(target=get_thread, args=(i + 1,)) for i in range(5)]
 t2=datetime.now()
-threads = [Thread(target=get_thread(i + 1), args=(i + 1)) for i in range(5)]
 for t in threads:
     t.start()
 for t in threads:
